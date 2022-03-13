aee763afd89923cd55dba3699770de064611e086
maciej <mb@4sys.pl>
2022-03-13 10:46:54 +0100

    add local/global time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,35 @@
 package com.company;
 
+import java.time.Instant;
+import java.time.LocalTime;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //SearchAlgoritm(); // pkt. 3
+
+        ShowTime();//pkt. 5
+    }
+
+    private static void ShowTime() {
+        LocalTime localTIme = LocalTime.now();
+        System.out.println("Local time :" + localTIme);
+
+        Instant globalTime = Instant.now();
+        System.out.println("Global time :" + globalTime);
+    }
+
+    private static void SearchAlgoritm()
+    {
         int[] array = {4, 5, 7, 11, 12, 15, 15, 21, 40, 45 };
-        int index = searchIndex(array, 11); // we want to find index for 11
+        int index = SearchIndex(array, 11); // we want to find index for 11
 
         System.out.println(index);
     }
 
-    private static int searchIndex(int[] array, int value)
+    private static int SearchIndex(int[] array, int value)
     {
         int index = 0;
         int limit = array.length - 1;
@@ -31,5 +50,18 @@ public class Main {
         }
 
         return -1;
+    }
+
+    private static void CRC32()
+    {
+//        int[] CRC_TABLE = new int[256];
+
+//        for (int i = 0; i < 256; ++i) {
+//            int code = i;
+//            for (int j = 0; j < 8; ++j) {
+//                code = (code & 0x01 ? 0xEDB88320 ^ (code >>> 1) : (code >>> 1));
+//            }
+//            CRC_TABLE[i] = code;
+//        }
     }
 }
